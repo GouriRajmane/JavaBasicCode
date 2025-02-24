37979a5a3ff75186d777cd649bab29a40a8099b9
Gouri <gouri@gmail.com>
2025-02-24 10:53:41 +0530

    updated Swapping Program

--- Swapping.java
+++ Swapping.java
@@ -6,6 +6,8 @@ public class Swapping {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		
+		//without using the third variable
+		
 		System.out.print("Enter value a:");
 		int a= sc.nextInt();
 		
@@ -13,11 +15,12 @@ public class Swapping {
 		int b= sc.nextInt();
 		System.out.print("Before Swapping:");
 		System.out.println("a:"+a + "b:"+b);
-		int t= a;
-		 a = b;
-		 b = t;
-		 System.out.print("Afterr Swapping:");
+		 a= a+b;
+		 b = a-b;
+		 a = a-b;
+		 System.out.print("After Swapping:");
 		 System.out.println("a:"+a  + "b:"+b);
+		 
 		
 		/*
 		int a=7, b=5;
@@ -27,9 +30,25 @@ public class Swapping {
 		 a = b;
 		 b = t;
 		
-		System.out.print("Afterr Swapping:");
+		System.out.print("After Swapping:");
 		System.out.println("a:"+a  + "b:"+b);
 		*/
+		 
+		/* Scanner sc= new Scanner(System.in);
+			
+			System.out.print("Enter value a:");
+			int a= sc.nextInt();
+			
+			System.out.print("Enter value b:");
+			int b= sc.nextInt();
+			System.out.print("Before Swapping:");
+			System.out.println("a:"+a + "b:"+b);
+			int t= a;
+			 a = b;
+			 b = t;
+			 System.out.print("After Swapping:");
+			 System.out.println("a:"+a  + "b:"+b);
+			 */
 		
 	}
 
